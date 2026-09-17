2f9cc5d3fe1ea1f3a1dc654dc09727d5452c65eb
SophieYAN-YEE1 <sophieyylin123@gmail.com>
2026-09-17 15:17:21 +0100

    Added insertion functions

--- pipeline/load.py
+++ pipeline/load.py
@@ -26,6 +26,64 @@ def get_db_connection() -> connection:
         return None
 
 
+def add_claims_to_database(conn: connection, data: list[tuple]) -> None:
+    """Inserts claims to the database"""
+    with conn.cursor() as cursor:
+        query = """
+            INSERT INTO claim
+                (claim, claim_url, publish_datetime, access_datetime, verdict_id, technique_id, summary, claim_embedding)
+            VALUES %s
+            ON CONFLICT (claim, publish_datetime, access_datetime)
+            DO NOTHING;
+        """
+
+        execute_values(cursor, query, data)
+        conn.commit()
+
+
+def add_claim_tags_to_database(conn: connection, data: list[tuple]) -> None:
+    """Inserts claim and tags to the database"""
+    with conn.cursor() as cursor:
+        query = """
+            INSERT INTO claim_tags
+                (claim_id, tag_id)
+            VALUES %s
+            ON CONFLICT (claim_id, tag_id)
+            DO NOTHING;
+        """
+
+        execute_values(cursor, query, data)
+        conn.commit()
+
+
+def add_source_to_database(conn: connection, data: list[tuple]) -> None:
+    """Inserts source to the database"""
+    with conn.cursor() as cursor:
+        query = """
+            INSERT INTO source
+                (source_url, source_verification, outlet_id)
+            VALUES %s;
+        """
+
+        execute_values(cursor, query, data)
+        conn.commit()
+
+
+def add_claim_source_to_database(conn: connection, data: list[tuple]) -> None:
+    """Inserts claim and source to the database"""
+    with conn.cursor() as cursor:
+        query = """
+            INSERT INTO claim_source
+                (claim_id, source_id)
+            VALUES %s
+            ON CONFLICT (claim_id, source_id)
+            DO NOTHING;
+        """
+
+        execute_values(cursor, query, data)
+        conn.commit()
+
+
 if __name__ == "__main__":
 
     # Set up:
@@ -37,7 +95,5 @@ if __name__ == "__main__":
     if conn is None:
         raise SystemExit(1)
     logging.info("Successfully connected to the database")
-
-    # TODO: Complete after transformation
 
     conn.close()
