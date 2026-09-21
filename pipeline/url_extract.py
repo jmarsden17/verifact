ca142d64ba65e0276783865a48319ede1c4729e3
SophieYAN-YEE1 <sophieyylin123@gmail.com>
2026-09-21 23:57:00 +0100

    Added headers to bypass some security

--- pipeline/url_extract.py
+++ pipeline/url_extract.py
@@ -3,12 +3,17 @@ Extracts the content of the URL a user submits as a claim.
 """
 import logging
 from newspaper import Article
+import requests
+from bs4 import BeautifulSoup
 
 
 def extract_url(url: str) -> dict:
     """Extracts the content from the url"""
     try:
-        article = Article(url)
+        header = {
+            'User-Agent': 'Disinformation'
+        }
+        article = Article(url, headers=header, request_timeout=10)
         article.download()
         article.parse()
 
@@ -29,3 +34,7 @@ def extract_url(url: str) -> dict:
 if __name__ == "__main__":
 
     logging.basicConfig(level=logging.INFO)
+
+    web = 'https://www.ft.com/content/697c5196-3aa4-4c59-bad9-a455bcc1858f?syn-25a6b1a6=1'
+
+    print(extract_url(web))
