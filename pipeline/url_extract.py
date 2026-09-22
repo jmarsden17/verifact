2cd8b56c9bcd82540f316a87703319068f3baf5e
SophieYAN-YEE1 <sophieyylin123@gmail.com>
2026-09-22 10:12:58 +0100

    Changed to trafilatura and added firecrawl

--- pipeline/url_extract.py
+++ pipeline/url_extract.py
@@ -1,40 +1,60 @@
 """
 Extracts the content of the URL a user submits as a claim.
 """
+from os import environ
 import logging
-from newspaper import Article
-import requests
-from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+import trafilatura
+from firecrawl import Firecrawl
 
 
-def extract_url(url: str) -> dict:
-    """Extracts the content from the url"""
+def extract_with_trafilatura(url: str) -> str:
+    """Scrapes web content with trafilatura"""
     try:
-        header = {
-            'User-Agent': 'Disinformation'
-        }
-        article = Article(url, headers=header, request_timeout=10)
-        article.download()
-        article.parse()
-
-        logging.info("Successfully extracted from URL")
-
-        return {
-            "title": article.title,
-            "text": article.text
-        }
-
+        downloaded = trafilatura.fetch_url(url)
+        result = trafilatura.extract(downloaded)
+        if result:
+            logging.info('Successfully extracted URL with Trafilatura')
+            return result
     except Exception as e:
-        logging.error("Cannot extract from URL")
-        return {
-            "error": e
-        }
+        logging.warning('Failed to extract with Trafilatura: %s', e)
+    return None
+
+
+def extract_with_firecrawl(url: str) -> str:
+    """Scrapes web content using FireCrawl"""
+    try:
+        app = Firecrawl(api_key=environ['FIRECRAWL_API_KEY'])
+        result = app.scrape(url)
+        if isinstance(result, dict) and 'markdown' in result:
+            logging.info("Successfully extracted URL with Firecrawl")
+            return result['markdown']
+    except Exception as e:
+        logging.warning('Failed to extract with FireCrawl: %s', e)
+    return None
+
+
+def extract_url(url: str) -> str:
+    """Extracts information from URL"""
+    logging.info("Attempt extraction with Trafilatura")
+    extraction = extract_with_trafilatura(url)
+    if extraction:
+        return extraction
+
+    logging.info("Attempt extraction with FireCrawl")
+    extraction = extract_with_firecrawl(url)
+    if extraction:
+        return extraction
+
+    logging.warning('Error: Cannot extract content with any scraper')
+    return None
 
 
 if __name__ == "__main__":
 
+    load_dotenv()
     logging.basicConfig(level=logging.INFO)
 
-    web = 'https://www.ft.com/content/697c5196-3aa4-4c59-bad9-a455bcc1858f?syn-25a6b1a6=1'
+    web = 'https://www.bbc.co.uk/news/articles/c6y0z4gv0le4o'
 
-    print(extract_url(web))
+    extract_url(web)
